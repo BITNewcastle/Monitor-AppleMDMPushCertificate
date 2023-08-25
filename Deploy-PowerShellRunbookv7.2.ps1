6d82d09e883a7fd6541e53779e6fd1a09990cfa4
BITNewcastle <137378485+BITNewcastle@users.noreply.github.com>
2023-08-25 14:59:21 +1000

    Update Deploy-PowerShellRunbookv7.2.ps1 - testing

--- Deploy-PowerShellRunbookv7.2.ps1
+++ Deploy-PowerShellRunbookv7.2.ps1
@@ -19,10 +19,15 @@ param(
   [string]$runbookName,
   [string]$location
 )
-# Define URL for PS script to be uploaded to runbook
-$scriptContent = 'https://cdn.jsdelivr.net/gh/BITNewcastle/Monitor-AppleMDMPushCertificate/Monitor-AppleMDMPushCertificate.ps1'
-# Connect to Azure AD with user-assigned managed identity
-#Connect-AzureAD -Identity
+
+#$resourceGroupName = 'test-applemdmcert_monitoring'
+#$automationAccountName = 'test-applemdmcert-monitoring'
+#$runbookName = 'test-applemdmcert-monitoring7.2'
+#$location = 'australiaeast'
+
+# Define PS script to be uploaded to runbook
+$scriptURL = 'https://cdn.jsdelivr.net/gh/BITNewcastle/Monitor-AppleMDMPushCertificate/Monitor-AppleMDMPushCertificate.ps1'
+$scriptContent = Invoke-RestMethod $scriptURL
 # Define variables for MS Graph API permissions #
 $resourceAppId = '00000003-0000-0000-c000-000000000000' # MS Graph application
 $permissionList = 'DeviceManagementServiceConfig.Read.All', 'Mail.Send' # Read MS Intune service properties, and send mail as user
@@ -33,25 +38,25 @@ $permissionList = 'DeviceManagementServiceConfig.Read.All', 'Mail.Send' # Read M
 ## Create PowerShell 7.2 runbook, upload script content and publish runbook ##
 # Create PowerShell 7.2 runbook 
 Invoke-AzRestMethod -Method "PUT" -ResourceGroupName $resourceGroupName -ResourceProviderName "Microsoft.Automation" `
-        -ResourceType "automationAccounts" -Name $runbookName -ApiVersion 2022-06-30-preview `
-        -Payload "{`"properties`":{`"runbookType`":`"PowerShell`",`"runtime`":`"PowerShell-7.2`", `"logProgress`":false, `"logVerbose`":false, `"draft`":{}}, `"location`":`"$($location)`"}"
+        -ResourceType "automationAccounts" -Name "${automationAccountName}/runbooks/${runbookName}" -ApiVersion  2022-06-30-preview `
+        -Payload "{`"properties`":{`"runbookType`":`"PowerShell`", `"runtime`":`"PowerShell-7.2`", `"logProgress`":false, `"logVerbose`":false, `"draft`":{}}, `"location`":`"$($location)`"}"
 # Upload PS script to runbook
 Invoke-AzRestMethod -Method "PUT" -ResourceGroupName $resourceGroupName -ResourceProviderName "Microsoft.Automation" `
-        -ResourceType "automationAccounts" -Name $runbookName -ApiVersion 2015-10-31 `
+        -ResourceType "automationAccounts" -Name "${automationAccountName}/runbooks/${runbookName}/draft/content" -ApiVersion 2015-10-31 `
         -Payload "$scriptContent"
 # Publish runbook
 Publish-AzAutomationRunbook -Name $runbookName -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
 
 ## Set required MS Graph API permissions for the automation account ##
-#$MSI = (Get-AzureADServicePrincipal -Filter "displayName eq '$automationAccountName'")
-#if (!$MSI) { throw "Automation account '$automationAccountName' doesn't exist" }
-#$resourceSP = Get-AzureADServicePrincipal -Filter "appId eq '$resourceAppId'"
-#if (!$resourceSP) { throw "Resource '$resourceAppId' doesn't exist" }
-#foreach ($permission in $permissionList) {
-#    $appRole = $resourceSP.AppRoles | Where-Object { $_.Value -eq $permission -and $_.AllowedMemberTypes -contains "Application" }
-#    if (!$appRole) {
-#        Write-Warning "Application permission '$permission' wasn't found in '$resourceAppId' application. Therefore it cannot be added."
-#        continue
-#    }
-#    New-AzureADServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $resourceSP.ObjectId -Id $appRole.Id
-#}
+$MSI = (Get-AzADServicePrincipal -Filter "displayName eq '$automationAccountName'")
+if (!$MSI) { throw "Automation account '$automationAccountName' doesn't exist" }
+$resourceSP = Get-AzADServicePrincipal -Filter "appId eq '$resourceAppId'"
+if (!$resourceSP) { throw "Resource '$resourceAppId' doesn't exist" }
+foreach ($permission in $permissionList) {
+    $appRole = $resourceSP.AppRoles | Where-Object { $_.Value -eq $permission -and $_.AllowedMemberTypes -contains "Application" }
+    if (!$appRole) {
+        Write-Warning "Application permission '$permission' wasn't found in '$resourceAppId' application. Therefore it cannot be added."
+        continue
+    }
+    New-AzureADServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $resourceSP.ObjectId -Id $appRole.Id
+}

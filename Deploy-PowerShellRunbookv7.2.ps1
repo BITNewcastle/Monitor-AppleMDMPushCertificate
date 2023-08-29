f0162caf39c2e35ed149bb97f8e7e2d5161a702c
BITNewcastle <137378485+BITNewcastle@users.noreply.github.com>
2023-08-29 12:09:26 +1000

    Update Deploy-PowerShellRunbookv7.2.ps1

--- Deploy-PowerShellRunbookv7.2.ps1
+++ Deploy-PowerShellRunbookv7.2.ps1
@@ -1,10 +1,10 @@
 <#
     .SYNOPSIS
-        Deploys PowerShell 7.2 Runbook under an existing Automation Account.
+        Deploys PowerShell 7 Runbook under an existing Automation Account.
 
     .DESCRIPTION
         Gets the content of the PS script to be deployed to runbook
-        Creates draft PS 7.2 runbook
+        Creates draft PowerShell 7 runbook
         Uploads PS script content to runbook
         Publishes runbook
         Deletes the user-assigned managed identity associated with this deployment script - cleans up
@@ -32,17 +32,42 @@ $scriptContent = Invoke-RestMethod $runbookScriptUri
 
 ####----------------[SCRIPT]----------------####
 
-## Create PowerShell 7.2 runbook, upload script content and publish runbook ##
-# Create PowerShell 7.2 runbook 
+## Import required PowerShell modules to existing automation account
+$moduleVersion = '1.28.0'
+$Modules = @()
+$Modules += "Microsoft.Graph.Authentication"
+$Modules += "Microsoft.Graph.Users.Actions"
+
+Foreach ($ModuleName in $Modules)
+{
+$Payload = @"
+    {"properties":
+        {"contentLink":
+            {"uri":"https://www.powershellgallery.com/api/v2/package/$ModuleName/$moduleVersion"}
+        }
+    }
+"@
+
+Invoke-AzRestMethod `
+    -Method PUT `
+    -ResourceGroupName $resourceGroupName -ResourceProviderName "Microsoft.Automation" `
+    -ResourceType "automationAccounts" -Name $automationAccountName/powershell7Modules/$ModuleName -ApiVersion 2022-08-08 `
+    -Payload $Payload
+}
+
+
+## Create PowerShell 7 runbook, upload script content and publish runbook ##
+# Create PowerShell 7 runbook 
 Invoke-AzRestMethod -Method "PUT" -ResourceGroupName $resourceGroupName -ResourceProviderName "Microsoft.Automation" `
         -ResourceType "automationAccounts" -Name "${automationAccountName}/runbooks/${runbookName}" -ApiVersion  2022-06-30-preview `
-        -Payload "{`"properties`":{`"runbookType`":`"PowerShell`", `"runtime`":`"PowerShell-7.2`", `"logProgress`":false, `"logVerbose`":false, `"draft`":{}}, `"location`":`"$($location)`"}"
+        -Payload "{`"properties`":{`"runbookType`":`"PowerShell`", `"runtime`":`"PowerShell-7`", `"logProgress`":false, `"logVerbose`":false, `"draft`":{}}, `"location`":`"$($location)`"}"
 # Upload PS script to runbook
 Invoke-AzRestMethod -Method "PUT" -ResourceGroupName $resourceGroupName -ResourceProviderName "Microsoft.Automation" `
         -ResourceType "automationAccounts" -Name "${automationAccountName}/runbooks/${runbookName}/draft/content" -ApiVersion 2015-10-31 `
         -Payload "$scriptContent"
 # Publish runbook
 Publish-AzAutomationRunbook -Name $runbookName -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
+
 
 # Outputs to allow for nested template
 $DeploymentScriptOutputs = @{}

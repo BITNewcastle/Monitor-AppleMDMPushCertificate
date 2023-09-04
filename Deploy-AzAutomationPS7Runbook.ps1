cef164e5f61959123d4467b1e589deb01d636806
BITNewcastle <137378485+BITNewcastle@users.noreply.github.com>
2023-09-04 16:22:16 +1000

    Update and rename Deploy-PowerShellRunbookv7.2.ps1 to Deploy-AzAutomationPS7Runbook.ps1

--- Deploy-AzAutomationPS7Runbook.ps1
+++ Deploy-AzAutomationPS7Runbook.ps1
@@ -1,12 +1,12 @@
 <#
     .SYNOPSIS
-        Deploys PowerShell 7.1 Runbook under an existing Automation Account.
+        Deploys PowerShell 7.2 Runbook under an existing Automation Account.
 
     .DESCRIPTION
         This script is deployed in the form of a deployment script, as part of a Bicep deployment.
         Import PowerShell module dependency to existing automation account first, before proceeding with rest of script.
         Imports remaining PowerShell module to existing automation account.
-        Creates draft PowerShell 7.1 runbook.
+        Creates draft PowerShell 7.2 runbook.
         Uploads specified PS script content to runbook.
         Publishes runbook.
         Deletes the user-assigned managed identity and role assigment associated with this deployment script - cleans up.
@@ -14,7 +14,6 @@
     .NOTES
         AUTHOR: Christopher Cooper
 #>
-
 
 #-------------------[INITIALISATIONS]-------------------#
 
@@ -32,7 +31,6 @@ param (
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['runbookName'] = $runbookName
 
-
 ####----------------[SCRIPT]----------------####
 
 ## Import PowerShell module dependency to existing automation account
@@ -47,13 +45,13 @@ function New-AzAutomationPS7ModuleDependency {
     Write-Output "Importing module ${ModuleName}"
     Invoke-AzRestMethod -Method 'PUT' `
         -ResourceGroupName $ResourceGroupName -ResourceProviderName 'Microsoft.Automation' `
-        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/powershell7Modules/$ModuleName" -ApiVersion 2022-08-08 `
+        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/powershell7Modules/$ModuleName" -ApiVersion '2019-06-01&runtimeVersion=7.2' `
         -Payload "{`"properties`":{`"contentLink`":{`"uri`":`'https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion`'}}}"
     # Wait until module is imported
     do {
         $Result = Invoke-AzRestMethod -Method 'GET' `
                     -ResourceGroupName $ResourceGroupName -ResourceProviderName 'Microsoft.Automation' `
-                    -ResourceType 'automationAccounts' -Name "$AutomationAccountName/powershell7Modules/$ModuleName" -ApiVersion 2022-08-08 `
+                    -ResourceType 'automationAccounts' -Name "$AutomationAccountName/powershell7Modules/$ModuleName" -ApiVersion '2019-06-01&runtimeVersion=7.2' `
                     -Payload "{`'properties`':{`'contentLink`':{`'uri`':`'https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion`'}}}"
         $ProvisioningState = (($Result.Content | ConvertFrom-Json).Properties).provisioningState
         Start-Sleep -Seconds 15
@@ -61,7 +59,7 @@ function New-AzAutomationPS7ModuleDependency {
             Write-Output "Import of module ${ModuleName} failed"
             break
         }
-    } until ($ProvisioningState -eq 'Succeeded')       
+    } until ($ProvisioningState -eq 'Succeeded')
 }
 
 ## Import remaining PowerShell module to existing automation account
@@ -76,11 +74,11 @@ function New-AzAutomationPS7Module {
     Write-Output "Importing module ${ModuleName}"
     Invoke-AzRestMethod -Method 'PUT' `
         -ResourceGroupName $ResourceGroupName -ResourceProviderName 'Microsoft.Automation' `
-        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/powershell7Modules/$ModuleName" -ApiVersion 2022-08-08 `
+        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/powershell7Modules/$ModuleName" -ApiVersion '2019-06-01&runtimeVersion=7.2' `
         -Payload "{`'properties`':{`'contentLink`':{`'uri`':`'https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion`'}}}"
 }
 
-## Create PowerShell 7 runbook, upload script content and publish runbook ##
+## Create PowerShell 7.2 runbook, upload script content and publish runbook ##
 function New-AzAutomationPS7Runbook {
     param (
         [string]$ResourceGroupName, 
@@ -90,17 +88,17 @@ function New-AzAutomationPS7Runbook {
     )
     # Get content of PS script
     $ScriptContent = Invoke-RestMethod $RunbookScriptUri
-    # Create PowerShell 7 runbook
-    Write-Output "Creating PowerShell 7 runbook ${RunbookName}"
+    # Create PowerShell 7.2 runbook
+    Write-Output "Creating PowerShell 7.2 runbook ${RunbookName}"
     Invoke-AzRestMethod -Method 'PUT' `
         -ResourceGroupName $ResourceGroupName -ResourceProviderName 'Microsoft.Automation' `
-        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/runbooks/$RunbookName" -ApiVersion 2022-08-08 `
-        -Payload "{`'properties`':{`'runbookType`':`'PowerShell7`', `'logProgress`':false, `'logVerbose`':false, `'draft`':{}}, `'location`':`'${Location}`'}"
+        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/runbooks/$RunbookName" -ApiVersion 2022-06-30-preview `
+        -Payload "{`'properties`':{`'runbookType`':`'PowerShell`', `"runtime`":`"PowerShell-7.2`", `'logProgress`':false, `'logVerbose`':false, `'draft`':{}}, `'location`':`'${Location}`'}"
     # Upload PS script to runbook
     Write-Output "Uploading ${RunbookScriptUri} to ${RunbookName}"
     Invoke-AzRestMethod -Method 'PUT' `
         -ResourceGroupName $ResourceGroupName -ResourceProviderName 'Microsoft.Automation' `
-        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/runbooks/$RunbookName/draft/content" -ApiVersion 2022-08-08 `
+        -ResourceType 'automationAccounts' -Name "$AutomationAccountName/runbooks/$RunbookName/draft/content" -ApiVersion 2022-06-30-preview `
         -Payload $ScriptContent
     # Publish runbook
     Write-Output "Publishing runbook ${RunbookName}"
